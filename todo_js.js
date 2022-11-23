1aee6131e6566cd1aa4b79954e8272bd970596b7
TH Carvalho <thiagocarvalhovita@gmail.com>
2022-11-23 11:42:02 -0300

    Update todo_js.js

--- todo_js.js
+++ todo_js.js
@@ -39,20 +39,15 @@ function dados() {
     document.getElementById('exibir').style.display = "none";
     document.getElementById('dados').style.display = "block";
 
-    var nomeDado = (localStorage.getItem('nome'));
-    var loginDado = (localStorage.getItem('login'));
-    var emailDado = (localStorage.getItem('email'));
-    var senhaDado = (localStorage.getItem('senha'));
-
     var p1 = document.getElementById("p1");
     var p2 = document.getElementById("p2");
     var p3 = document.getElementById("p3");
     var p4 = document.getElementById("p4");
 
-    p1.innerText = "Nome: " + nomeDado;
-    p2.innerText = "Login: " + loginDado;
-    p3.innerText = "Email: " + emailDado;
-    p4.innerText = "Senha: " + senhaDado;
+    p1.innerText = "Nome: " + nomeSalvo;
+    p2.innerText = "Login: " + loginSalvo;
+    p3.innerText = "Email: " + emailSalvo;
+    p4.innerText = "Senha: " + senhaSalva;
 }
 
 document.replaceChild(exibir, cadastrar, dados);
@@ -62,21 +57,23 @@ function nomeValido(){
     if(nome.value == "")
     {
         nome.style.backgroundColor="#FFFFFF";
+        nome.style.transitionDuration="0.3s";
         span[0].style.display="none";
     }
     else if(nome.value.length < 10)
     {
         nome.style.backgroundColor="#FF9C9D";
+        nome.style.transitionDuration="0.3s";
         span[0].style.display="block";
         span[0].style.transitionDuration="0.3s";
     }
     else
     {
         nome.style.backgroundColor="#92FFC0";
+        nome.style.transitionDuration="0.3s";
         span[0].style.display="none";
         span[0].style.transitionDuration="0.3s";
-        var nomeSave = document.querySelector("#nome").value;
-        localStorage.setItem('nome', nomeSave);
+        var nomeSalvo = document.getElementById("nome").value;
     }
 }
 
@@ -84,19 +81,21 @@ function loginValido(){
     if(login.value == "")
     {
         login.style.backgroundColor="#FFFFFF";
+        login.style.transitionDuration="0.3s";
         span[1].style.display="none";
     }
     else if(login.value.length > 10)
     {
         login.style.backgroundColor="#FF9C9D";
+        login.style.transitionDuration="0.3s";
         span[1].style.display="block";
     }
     else
     {
         login.style.backgroundColor="#92FFC0";
+        login.style.transitionDuration="0.3s";
         span[1].style.display="none";
-        var loginSave = document.querySelector("#login").value;
-        localStorage.setItem('login', loginSave);
+        var loginSalvo = document.getElementById("login").value;
     }
 }
 
@@ -104,19 +103,21 @@ function mailValido(){
     if(email.value == "")
     {
         email.style.backgroundColor="#FFFFFF";
+        email.style.transitionDuration="0.3s";
         span[2].style.display="none";
     }
     else if(!emailRegex.test(email.value))
     {
         email.style.backgroundColor="#FF9C9D";
+        email.style.transitionDuration="0.3s";
         span[2].style.display="block";
     }
     else
     {
         email.style.backgroundColor="#92FFC0";
+        email.style.transitionDuration="0.3s";
         span[2].style.display="none";
-        var emailSave = document.querySelector("#email").value;
-        localStorage.setItem('email', emailSave);
+        var emailSalvo = document.getElementById("email").value;
     }
 }
 
@@ -124,24 +125,34 @@ function senhaValida(){
     if(senha.value == "" && cfsenha.value == "" )
     {
         senha.style.backgroundColor="#FFFFFF";
+        senha.style.transitionDuration="0.3s";
         cfsenha.style.backgroundColor="#FFFFFF";
+        cfsenha.style.transitionDuration="0.3s";
         span[3].style.display="none";
         span[4].style.display="none";
     }
     else if(senha.value == cfsenha.value)
     {
         senha.style.backgroundColor="#92FFC0";
+        senha.style.transitionDuration="0.3s";
         cfsenha.style.backgroundColor="#92FFC0";
+        cfsenha.style.transitionDuration="0.3s";
         span[3].style.display="none";
         span[4].style.display="none";
-        var senhaSave = document.querySelector("#senha").value;
-        localStorage.setItem('senha', senhaSave);
+        var senhaSalva = document.getElementById("senha").value;
     }
     else
     {
         senha.style.backgroundColor="#FF9C9D";
+        senha.style.transitionDuration="0.3s";
         cfsenha.style.backgroundColor="#FF9C9D";
+        cfsenha.style.transitionDuration="0.3s";
         span[3].style.display="block";
         span[4].style.display="block";
     }
+}
+
+function botaoDados(){
+    document.getElementById("cadastro_dados").display="none";
+    document.getElementById("conteudo").display="block";
 }
